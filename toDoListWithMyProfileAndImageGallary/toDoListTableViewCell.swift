3b5a6dbdd93b8ad46e01aee659520288b60b60d3
rockvivek <vinodkumarshrivastwa@gmail.com>
2021-02-15 10:29:39 +0530

    toDoListWithGallaryAndProfile

---
+++ toDoListWithMyProfileAndImageGallary/toDoListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  toDoListTableViewCell.swift
+//  toDoListWithMyProfileAndImageGallary
+//
+//  Created by IPHTECH 20 on 11/02/21.
+//  Copyright © 2021 iPHTech20. All rights reserved.
+//
+
+import UIKit
+
+ 
+class toDoListTableViewCell: UIViewController {
+    override func viewDidLoad() {
+         super.viewDidLoad()
+       
+    }
+
+    @IBOutlet weak var priorityTextField: UITextField!
+    @IBOutlet weak var descriptiontextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+          
+    @IBAction func AddButton(_ sender: UIButton) {
+        toDoListRemDataArray.append(toDoListFormat(title: titleTextField.text!, description: descriptiontextField.text!, isPriorityTask: Int(priorityTextField.text!)!))
+        let viewControllerObj = storyboard?.instantiateViewController(identifier: "ViewController") as! ViewController
+        self.navigationController?.pushViewController(viewControllerObj, animated: true)
+    }
+}
